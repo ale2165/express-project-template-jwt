97e626f9422ea648f2c3c0b145d1c2e4fb8c7b92
Alessandro Liberatoscioli <alessandro.liberatoscioli.g@gmail.com>
2024-03-20 19:48:37 +0100

    primera rama

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 const morgan = require('morgan');
-const cors = require('cors')
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
+const authRouter = require("./routes/auth");
+const homeRouter = require("./routes/home");
+const indexRouter = require("./routes/index");
+const profileRouter = require("./routes/profile");
 
-const whitelist = ['http://localhost:5173']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true,
-}
-app.use(cookieParser());
-app.use(cors(corsOptions))
+// Middlewares
 app.use(morgan('dev'));
-
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
-require('./config/passport');
-require('./config/cloudinary');
+// Rutas
+app.use("/auth", authRouter);
+app.use("/home", homeRouter);
+app.use("/index", indexRouter);
+app.use("/profile", profileRouter);
 
-app.use("/", require("./routes"));
+// Manejo de errores
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// Puerto de escucha
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

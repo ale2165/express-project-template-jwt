e6b6c067b66ba97b545371041b84be75549598b4
Alessandro Liberatoscioli <alessandro.liberatoscioli.g@gmail.com>
2024-03-21 22:06:28 +0100

    act prisma

--- prisma/index.js
+++
@@ -1,6 +0,0 @@
-// Sin este fichero no podemos utilizar prisma en el resto de la aplicación
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
-
-module.exports = prisma;
